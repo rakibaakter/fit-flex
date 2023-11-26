c5753b334c7d2dc841005ec75c8bba52ecf2acda
rakibaakter <rakibaakter635@gmail.com>
2023-11-26 16:28:18 +0600

    project initiate

---
+++ src/Routers/Routers.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <>gfdshi</>,
+  },
+]);
+
+export default Routers;
