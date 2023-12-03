94327dc0c9efe7f00498769a3705661699c4f182
rakibaakter <rakibaakter635@gmail.com>
2023-12-03 14:57:06 +0600

    modified ui

--- src/Pages/Classes/ClassComponent/WeekClass/WeekClass.jsx
+++ src/Pages/Classes/ClassComponent/WeekClass/WeekClass.jsx
@@ -14,18 +14,18 @@ const WeekClass = ({ classes }) => {
   ];
 
   return (
-    <div className="-mt-20 lg:-mt-28">
+    <div className="-mt-20  md:-mt-28">
       <div className="text-center ">
         <h2 className="btn border-none rounded-none rounded-t-2xl px-6 text-red-300 md:text-xl font-bold">
           Weekly Schedule
         </h2>
       </div>
       <div className="overflow-x-auto">
-        <table className="table bg-red-300">
+        <table className="table bg-transparent  ">
           <thead>
             <tr className=" md:text-2xl text-red-600 text-center ">
               {days.map((day) => (
-                <th key={day}>
+                <th key={day} className="border ">
                   <p>{day}</p>
                 </th>
               ))}
@@ -34,13 +34,13 @@ const WeekClass = ({ classes }) => {
           <tbody>
             <tr>
               {days.map((day) => (
-                <td key={day} className="border bg-red-300">
+                <td key={day} className="border ">
                   {classes
                     .filter((item) => item.day === day)
                     .map((classItem) => (
                       <div
                         key={classItem._id}
-                        className="mb-2 w-full md:text-xl font-semibold text-black bg-white px-2 py-2 text-center "
+                        className="mb-2 w-full md:text-xl font-semibold  bg-red-300 px-2 py-2 text-center "
                       >
                         <p>{classItem.name}</p>
                       </div>
