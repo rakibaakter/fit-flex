53123bae3f2133a704feee7686068bc30da2c171
rakibaakter <rakibaakter635@gmail.com>
2023-12-05 13:56:48 +0600

    added trainer info table

--- src/Dashboard/AdminAccess/AllTrainers/AllTrainers.jsx
+++ src/Dashboard/AdminAccess/AllTrainers/AllTrainers.jsx
@@ -12,11 +12,50 @@ const AllTrainers = () => {
       return result.data;
     },
   });
-  //   console.log(trainers);
+  console.log(trainers);
 
   return (
     <div>
-      <SectionTitle value="All Trainers" />
+      <section>
+        <SectionTitle value="All Trainers" />
+        <h2 className="uppercase md:text-2xl font-medium r mb-12 ">
+          Total Trainers : {trainers.length}
+        </h2>
+
+        <div>
+          <div className="overflow-x-auto rounded-t-lg">
+            <table className="table ">
+              {/* head */}
+              <thead className="bg-red-300 border">
+                <tr>
+                  <th className="border"></th>
+                  <th className="border">Name</th>
+                  <th className="border">Email</th>
+                  <th className="border">Payment</th>
+                </tr>
+              </thead>
+              <tbody>
+                {trainers.map((trainer, index) => (
+                  <tr key={trainer._id} className="border">
+                    <th className="border">
+                      <p>{index + 1}</p>
+                    </th>
+                    <td className="border">
+                      <h2 className="font-medium">{trainer.trainerName}</h2>
+                    </td>
+                    <td className="border">
+                      <h2 className="font-medium">{trainer.email}</h2>
+                    </td>
+                    <th className="border">
+                      <button className="btn ">Pay</button>
+                    </th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
